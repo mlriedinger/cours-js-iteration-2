742130e8ce13b5f5636dacc184930a5931138ac1
Marie-Laure <marie-laure.riedinger@le-campus-numerique.fr>
2020-03-27 15:10:36 +0100

    Etape 5 (cheat jQuery)
    Désolée mais je suis trop contente que ça marche ^^
    Maintenant j'essaie en Vanilla

--- public/static/js/objects.js
+++ public/static/js/objects.js
@@ -9,20 +9,27 @@ function load_components(){
     console.log("Chargement des données de la page");
     $.get('http://localhost:5000/objects', function(data){
         for (let element of data.objects){
+            // console.log(element);
             add_line_to_table(element);
         }
     })
-    // Ajouter ici le code permettant de charger dynamiquement les éléments de la page
 }
 
 // Solution 1 pour appeler la fonction load_components()
-
 // load_components();
 
 function add_line_to_table(data){
+    let image;
+    if (data.image == undefined){
+        // console.log(data.type);
+        load_default_image(data.type, data.serial);
+        // console.log(load_default_image(data.type));
+    }
+    else image = data.image;
+
     let line =  '<tr>\
                     <td>' + data.serial + '</td>\
-                    <td><img src="./static/images/' + data.image + '"/></td>\
+                    <td><img src="./static/images/' + image + '"/></td>\
                     <td>' + data.description + '</td>\
                     <td><input type="checkbox"></td>\
                     <td><input type="button" class="btn btn-dark" value="Détails"></td>\
@@ -45,3 +52,25 @@ let object_data = {
         "operator": "JPA"
         }
    }
+   
+function load_default_image(type, serial){
+    
+    $.get('http://localhost:5000/data', function(data){
+      
+    let image;
+        for (let element in data.types){
+            if (element == type){
+                // console.log(data.types[element].default_image);
+                image = data.types[element].default_image;
+                // console.log(image);
+                $('td:contains('+serial+')').next().children().attr('src', '/static/images/'+image);
+                console.log('Data Serial :' + serial);
+                console.log('Data image :' + image);
+                // console.log(document.getElementsByTagName(`td:contains(${data.objects.serial})`));
+                return image;
+            }
+        }   
+    });
+}
+
+// load_default_image('Digital_CO2');
